7623114e67f6d825751a3d85cb8122bc81f14816
eldor togaymurotov <techmaktab@gmail.com>
2022-12-14 23:15:22 -0500

    calculator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import React, {useState} from "react";
+import { Container } from 'react-bootstrap';
 
 function App() {
+const [calculation, setCalculation]=useState('')
+    const [output, setOutput]=useState('')
+    const actions = ['/', '*', '+', '-', '.']
+    const updateCalculation=value=>{
+
+    if(
+        actions.includes(value) & calculation === '' ||
+        actions.includes(value) & actions.includes(calculation.slice(-1))
+    ){
+        return
+    }
+    setCalculation(calculation+value)
+
+        if (!actions.includes(value)){
+            setOutput(eval(calculation+value).toString())
+        }
+
+    }
+   const createDigits =()=>{
+       const digits=[]
+
+       for (let i=1;i<10; i++){
+           digits.push(
+               <button onClick={()=>updateCalculation(i.toString())} key={i}>{i}</button>
+           )
+       }
+       return digits
+   }
+const calculate=()=>{
+    setCalculation(eval(calculation).toString())
+}
+    const clear=()=>{
+        if(calculation === ''){
+            return
+        }
+        const value=calculation.slice(0,-1)
+        setCalculation(value)
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <div className='calculator-body'>
+        <br />
+    <center> <h1 className='h1'>Calculator</h1></center>
+
+          <div className="calc-grid">
+      <div className='output'>
+          {calculation||'0'}
+          {output ? <span className="preRes">{output}</span>:''}
+
+
+      </div>
+
+<div>
+        <div className='ops'>
+
+        <button className='cal-btn' onClick={()=>{updateCalculation('/')}} >/</button>
+        <button className='cal-btn' onClick={()=>{updateCalculation('*')}}>*</button>
+        <button className='cal-btn' onClick={()=>{updateCalculation('+')}}>+</button>
+        <button className='cal-btn' onClick={()=>{updateCalculation('-')}}>-</button>
+        <button className='cal-btn' onClick={clear}> <img  width={40} height={40} src="https://cdn-icons-png.flaticon.com/512/159/159805.png" /></button>
+        </div>
+        <div className='dig'>
+
+            {createDigits()}
+
+        <button className='cal-btn' onClick={()=>{updateCalculation('.')}}>.</button>
+        <button className='cal-btn' onClick={()=>{updateCalculation('0')}}>0</button>
+        <button className='cal-btn' onClick={calculate}>=</button>
     </div>
+    </div>
+
+
+    </div>
+      </div>
+      </Container>
   );
 }
 
